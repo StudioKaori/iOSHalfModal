23060cd2b45dbe1e698b8ed6af9896439e1f266c
Studio Kaori <studiokaori.com@gmail.com>
2022-05-05 10:49:17 +0200

    Add: UIButton

--- HalfModal/ViewController.swift
+++ HalfModal/ViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("OPEN", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 8.0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        MSLayoutConstraint.activate([
+            button.heightAnchor.constraint(equalToConstant: 30.0),
+            button.widthAnchor.constraint(equalToConstant: 60.0)
+        ])
+        return button
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
