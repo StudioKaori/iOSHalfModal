d4caedceff5117df33a17596634be4eb85fb4181
Studio Kaori <studiokaori.com@gmail.com>
2022-05-05 10:42:17 +0200

    Initial Commit

---
+++ HalfModal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HalfModal
+//
+//  Created by Kaori Persson on 2022-05-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
